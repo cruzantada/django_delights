ad9233c2be5bb27901b999223e3bb03682e78e97
Anthony Cruz <cruzantada@gmail.com>
2022-02-01 23:45:36 -0600

    Add models for Ingredient, MenuItem, RecipeRequirement, and Purchase

--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+
+class Ingredient(models.Model):
+    """
+    Represents an ingredient in the restaurant's inventory
+    """
+
+    name = models.CharField(max_length=100, unique=True)
+    quantity = models.FloatField(default=0)
+    unit = models.CharField(max_length=100)
+    unit_price = models.FloatField(default=0.00)
+
+
+class MenuItem(models.Model):
+    """
+    Represents an item on the restaurant's menu
+    """
+
+    title = models.CharField(max_length=100, unique=True)
+    price = models.FloatField(default=0.00)
+
+
+class RecipeRequirement(models.Model):
+    """
+    Represents an ingredient and how much of it is required for an item on the restaurant's menu
+    """
+
+    menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    quantity = models.FloatField(default=0)
+
+
+class Purchase(models.Model):
+    """
+    Represents a customer purchase of an item on the restaurant's menu
+    """
+
+    menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
