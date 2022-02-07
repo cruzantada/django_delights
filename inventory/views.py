894527136908c93e4a0b5b4ab504aec173047fa2
Anthony Cruz <cruzantada@gmail.com>
2022-02-06 23:19:29 -0600

    Add initial views for ingredients, menu, and purchases

--- inventory/views.py
+++ inventory/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from .models import Ingredient, MenuItem, Purchase
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView
+from django.views.generic.edit import DeleteView
 
-# Create your views here.
+
+@login_required
+def home(request):
+    context = {"name": request.user}
+    return render(request, "inventory/home.html", context)
+
+
+class IngredientList(LoginRequiredMixin, ListView):
+    model = Ingredient
+    template_name = "inventory/ingredient_list.html"
+
+
+class IngredientDelete(DeleteView):
+    model = Ingredient
+    template_name = "inventory/ingredient_delete_form.html"
+
+
+class MenuItemList(LoginRequiredMixin, ListView):
+    model = MenuItem
+    template_name = "inventory/menu_item_list.html"
+
+
+class PurchaseList(LoginRequiredMixin, ListView):
+    model = Purchase
+    template_name = "inventory/purchase_list.html"
